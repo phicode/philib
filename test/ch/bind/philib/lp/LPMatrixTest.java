56ba5768792e8bc6025cce9b003b12a1e50bbb51
philipp <philipp@71d118b0-3217-0410-8c98-bf3badd46c86>
2009-12-13 21:57:19 +0000

    license-header updates

--- test/ch/bind/philib/lp/LPMatrixTest.java
+++ test/ch/bind/philib/lp/LPMatrixTest.java
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2009 Philipp Meinen <philipp@bind.ch>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software
+ * is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+ * THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package ch.bind.philib.lp;
 
 import static org.junit.Assert.assertEquals;
@@ -22,7 +44,7 @@ public class LPMatrixTest {
 		matrix.setTargetFunction(new double[] { 100, 250, 0 });
 
 		System.out.println(matrix);
-		
+
 		// STEP 1
 		MatrixPoint pivot = matrix.findPivot();
 		assertNotNull(pivot);
@@ -42,7 +64,7 @@ public class LPMatrixTest {
 		result = matrix.transform(pivot);
 		assertEquals(4960.0, result, 0.0);
 		System.out.println(matrix);
-		
+
 		// STEP 3
 		pivot = matrix.findPivot();
 		assertNotNull(pivot);
@@ -52,7 +74,7 @@ public class LPMatrixTest {
 		result = matrix.transform(pivot);
 		assertEquals(5400.0, result, 0.0);
 		System.out.println(matrix);
-		
+
 		// NO STEP 4
 		pivot = matrix.findPivot();
 		assertNull(pivot);
