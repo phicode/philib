c20f0b5716f2a0d5c5cd197bb3bfa101e0387ed0
philipp <philipp@71d118b0-3217-0410-8c98-bf3badd46c86>
2011-09-29 19:35:31 +0000

    add int64 encoding and decoding for big-endiang and little-endian

--- src/main/java/ch/bind/philib/io/EndianConverter.java
+++ src/main/java/ch/bind/philib/io/EndianConverter.java
@@ -22,6 +22,75 @@
 
 package ch.bind.philib.io;
 
-class EndianConverter {
+public final class EndianConverter {
 
+    public EndianConverter() {
+    }
+
+    // #################
+    // # ENCODE INT 64 #
+    // #################
+
+    public static void encodeInt64BE(long value, byte[] output) {
+        encodeInt64BE(value, output, 0);
+    }
+
+    public static void encodeInt64LE(long value, byte[] output) {
+        encodeInt64LE(value, output, 0);
+    }
+
+    public static void encodeInt64BE(long value, byte[] output, int offset) {
+        output[offset + 0] = (byte) (value >> 56 & 0xFF);
+        output[offset + 1] = (byte) (value >> 48 & 0xFF);
+        output[offset + 2] = (byte) (value >> 40 & 0xFF);
+        output[offset + 3] = (byte) (value >> 32 & 0xFF);
+        output[offset + 4] = (byte) (value >> 24 & 0xFF);
+        output[offset + 5] = (byte) (value >> 16 & 0xFF);
+        output[offset + 6] = (byte) (value >> 8 & 0xFF);
+        output[offset + 7] = (byte) (value & 0xFF);
+    }
+
+    public static void encodeInt64LE(long value, byte[] output, int offset) {
+        output[offset + 0] = (byte) (value & 0xFF);
+        output[offset + 1] = (byte) (value >> 8 & 0xFF);
+        output[offset + 2] = (byte) (value >> 16 & 0xFF);
+        output[offset + 3] = (byte) (value >> 24 & 0xFF);
+        output[offset + 4] = (byte) (value >> 32 & 0xFF);
+        output[offset + 5] = (byte) (value >> 40 & 0xFF);
+        output[offset + 6] = (byte) (value >> 48 & 0xFF);
+        output[offset + 7] = (byte) (value >> 56 & 0xFF);
+    }
+
+    // #################
+    // # DECODE INT 64 #
+    // #################
+    public static long decodeInt64BE(byte[] input) {
+        return decodeInt64BE(input, 0);
+    }
+
+    public static long decodeInt64LE(byte[] input) {
+        return decodeInt64LE(input, 0);
+    }
+
+    public static long decodeInt64BE(byte[] input, int offset) {
+        return ((input[offset + 0] & 0xFF) << 56) | //
+                ((input[offset + 1] & 0xFF) << 48) | //
+                ((input[offset + 2] & 0xFF) << 40) | //
+                ((input[offset + 3] & 0xFF) << 32) | //
+                ((input[offset + 4] & 0xFF) << 24) | //
+                ((input[offset + 5] & 0xFF) << 16) | //
+                ((input[offset + 6] & 0xFF) << 8) | //
+                (input[offset + 7] & 0xFF);
+    }
+
+    public static long decodeInt64LE(byte[] input, int offset) {
+        return (input[offset + 0] & 0xFF) | //
+                ((input[offset + 1] & 0xFF) << 8) | //
+                ((input[offset + 2] & 0xFF) << 16) | //
+                ((input[offset + 3] & 0xFF) << 24) | //
+                ((input[offset + 4] & 0xFF) << 32) | //
+                ((input[offset + 5] & 0xFF) << 40) | //
+                ((input[offset + 6] & 0xFF) << 48) | //
+                ((input[offset + 7] & 0xFF)) << 56;
+    }
 }
