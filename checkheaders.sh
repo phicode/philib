0af95bbcb3543610c983261d653d19aa4b352a2e
philipp <philipp@71d118b0-3217-0410-8c98-bf3badd46c86>
2011-10-23 19:17:05 +0000

    also check for the copyright year in the checkheaders script

--- checkheaders.sh
+++ checkheaders.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 EXPECT=" * Copyright (c)"
+FILTER="2011"
 LEXPECT=${#EXPECT}
 
 for file in $(find . -type f -name "*.java"); do
 
-	line2=$(head -n 2 "${file}" | tail -n 1)
+	line2=$(head -n 2 "${file}" | tail -n 1 | grep -E ${FILTER})
 	l2start=${line2:0:${LEXPECT}}
 	if [ "${EXPECT}" != "${l2start}" ]; then
 		echo "no copyright notice in: $file"
