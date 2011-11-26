be2bb62a6de2b41a5f3d25c28f5f04d6b9acba85
philipp <philipp@71d118b0-3217-0410-8c98-bf3badd46c86>
2011-11-26 16:08:42 +0000

    fix the private-constructor-test on windows

--- src/test/java/ch/bind/philib/PrivateConstructorTest.java
+++ src/test/java/ch/bind/philib/PrivateConstructorTest.java
@@ -27,9 +27,9 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Modifier;
 import java.net.URL;
 import java.util.Enumeration;
-import java.util.regex.Pattern;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 // this is here to satisfy the code-coverage tool emma
 // by invoking the private constructors.
@@ -37,102 +37,107 @@ import org.junit.Test;
 // test coverage, even utility classes.
 public class PrivateConstructorTest {
 
-    @Test
-    public void invokeAllPrivateCtors() throws Exception {
-        Package rootPackage = getClass().getPackage();
-        String packageName = rootPackage.getName();
-        String dot = Pattern.quote(".");
-        String resourceRootPath = packageName.replaceAll(dot, File.separator);
-        // System.out.println(rootPackage.getName());
-        // System.out.println(resourceRootPath);
-        Enumeration<URL> urls = getClass().getClassLoader().getResources(resourceRootPath);
-        while (urls.hasMoreElements()) {
-            URL url = urls.nextElement();
-            File path = new File(url.getPath());
-            recursiveTestPrivateCtors(packageName, path, 1);
-        }
-    }
+	@Test
+	public void invokeAllPrivateCtors() throws Exception {
+		Package rootPackage = getClass().getPackage();
+		String packageName = rootPackage.getName();
+		// resource paths use forward slash / as separator, on all platforms
+		String resourceRootPath = packageName.replace('.', '/');
+		System.out.printf("resourceRootPath=%s%n", resourceRootPath);
+		// System.out.println(rootPackage.getName());
+		// System.out.println(resourceRootPath);
+		Enumeration<URL> urls = getClass().getClassLoader().getResources(resourceRootPath);
+		while (urls.hasMoreElements()) {
+			URL url = urls.nextElement();
+			File path = new File(url.getPath());
+			recursiveTestPrivateCtors(packageName, path, 1);
+		}
+	}
 
-    private void recursiveTestPrivateCtors(String packageName, File path, int lvl) throws Exception {
-        // System.out.println("checking dir " + file);
-        File[] subPaths = path.listFiles();
-        int nextLvl = lvl + 1;
-        for (File subPath : subPaths) {
-            if (subPath.isDirectory()) {
-                String subPackageName = packageName + '.' + subPath.getName();
-                recursiveTestPrivateCtors(subPackageName, subPath, nextLvl);
-            } else if (subPath.isFile()) {
-                maybeTestFile(packageName, subPath);
-            } else {
-                System.out.println("unknown file type: " + subPath);
-            }
-        }
-    }
+	private void recursiveTestPrivateCtors(String packageName, File path, int lvl) throws Exception {
+		// System.out.println("checking dir " + file);
+		File[] subPaths = path.listFiles();
+		assertNotNull(path + ".listFiles() == null", subPaths);
+		int nextLvl = lvl + 1;
+		for (File subPath : subPaths) {
+			if (subPath.isDirectory()) {
+				String subPackageName = packageName + '.' + subPath.getName();
+				recursiveTestPrivateCtors(subPackageName, subPath, nextLvl);
+			} else if (subPath.isFile()) {
+				maybeTestFile(packageName, subPath);
+			} else {
+				System.out.println("unknown file type: " + subPath);
+			}
+		}
+	}
 
-    private static String TEST_FILE_SUFFIX = "Test.class";
+	private static String TEST_FILE_SUFFIX = "Test.class";
 
-    private void maybeTestFile(String packageName, File subPath) throws Exception {
-        // System.out.println("file: " +subFile);
-        String name = subPath.getName();
-        boolean doCheck = name.endsWith(".class") && !name.endsWith(TEST_FILE_SUFFIX);
-        if (doCheck) {
-            // strip away '.class'
-            String className = name.substring(0, name.length() - 6);
-            String fullClassName = packageName + '.' + className;
-            System.out.println("checking " + className + " => " + fullClassName + " => " + subPath);
-            Class<?> clazz = Class.forName(fullClassName);
-            maybeCheckClass(clazz);
-        }
-    }
+	private void maybeTestFile(String packageName, File subPath) throws Exception {
+		// System.out.println("file: " +subFile);
+		String name = subPath.getName();
+		boolean doCheck = name.endsWith(".class") && !name.endsWith(TEST_FILE_SUFFIX);
+		if (doCheck) {
+			// strip away '.class'
+			String className = name.substring(0, name.length() - 6);
+			String fullClassName = packageName + '.' + className;
+			System.out.println("checking " + className + " => " + fullClassName + " => " + subPath);
+			Class<?> clazz = Class.forName(fullClassName);
+			maybeCheckClass(clazz);
+		}
+	}
 
-    private void maybeCheckClass(Class<?> clazz) throws Exception {
-        if (clazz.isInterface()) {
-            System.out.println("ignoring because interface");
-            return;
-        }
-        if (clazz.isEnum()) {
-            System.out.println("ignoring because enum");return;
-        }
-        if (clazz.isAnnotation()) {
-            System.out.println("ignoring because annotation");return;
-        }
-        if (clazz.isLocalClass()) {
-            System.out.println("ignoring because local class");return;
-        }
-        //TODO: inner classes, anonymous classes
-        Constructor<?>[] ctors = clazz.getDeclaredConstructors();
-        if (ctors.length < 1) {
-            System.out.println("WTF?");
-        }
-        for (Constructor<?> ctor : ctors) {
-            if (ctor.isSynthetic()) {
-                // introduced by the compiler
-                System.out.println("ignoring because synthetic");
-            } else {
-                int modifiers = ctor.getModifiers();
-                if ((modifiers & Modifier.PRIVATE) == Modifier.PRIVATE) {
-                    int numParams = ctor.getParameterTypes().length;
-                    if (numParams == 0) {
-                        testCtor(ctor);
-                    } else {
-                        System.out.println("ignoring because not 0 parameters");
-                    }
-                } else {
-                    System.out.println("ignoring because not private");
-                }
-            }
-        }
-    }
+	private void maybeCheckClass(Class<?> clazz) throws Exception {
+		if (clazz.isInterface()) {
+			System.out.println("ignoring because interface");
+			return;
+		}
+		if (clazz.isEnum()) {
+			System.out.println("ignoring because enum");
+			return;
+		}
+		if (clazz.isAnnotation()) {
+			System.out.println("ignoring because annotation");
+			return;
+		}
+		if (clazz.isLocalClass()) {
+			System.out.println("ignoring because local class");
+			return;
+		}
+		// TODO: inner classes, anonymous classes
+		Constructor<?>[] ctors = clazz.getDeclaredConstructors();
+		if (ctors.length < 1) {
+			System.out.println("WTF?");
+		}
+		for (Constructor<?> ctor : ctors) {
+			if (ctor.isSynthetic()) {
+				// introduced by the compiler
+				System.out.println("ignoring because synthetic");
+			} else {
+				int modifiers = ctor.getModifiers();
+				if ((modifiers & Modifier.PRIVATE) == Modifier.PRIVATE) {
+					int numParams = ctor.getParameterTypes().length;
+					if (numParams == 0) {
+						testCtor(ctor);
+					} else {
+						System.out.println("ignoring because not 0 parameters");
+					}
+				} else {
+					System.out.println("ignoring because not private");
+				}
+			}
+		}
+	}
 
-    private void testCtor(Constructor<?> ctor) throws Exception {
-        Object o;
-        if (!ctor.isAccessible()) {
-            ctor.setAccessible(true);
-            o = ctor.newInstance(new Object[0]);
-            ctor.setAccessible(false);
-        } else {
-            o = ctor.newInstance();
-        }
-        System.out.println("instantiated a " + o.getClass().getSimpleName());
-    }
+	private void testCtor(Constructor<?> ctor) throws Exception {
+		Object o;
+		if (!ctor.isAccessible()) {
+			ctor.setAccessible(true);
+			o = ctor.newInstance(new Object[0]);
+			ctor.setAccessible(false);
+		} else {
+			o = ctor.newInstance();
+		}
+		System.out.println("instantiated a " + o.getClass().getSimpleName());
+	}
 }
