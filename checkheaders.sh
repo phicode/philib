c594748e2b17fbe899332445edb4304baf071316
philipp <philipp@71d118b0-3217-0410-8c98-bf3badd46c86>
2012-03-05 21:44:18 +0000

    make the checkheaders script not look for the copyright date since the copyright notice starts at the year of first publishing

--- checkheaders.sh
+++ checkheaders.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-EXPECT=" * Copyright (c)"
-FILTER="2011"
+EXPECT=" * Copyright (c) "
 LEXPECT=${#EXPECT}
 
 for file in $(find . -type f -name "*.java"); do
 
-	line2=$(head -n 2 "${file}" | tail -n 1 | grep -E ${FILTER})
+	line2=$(head -n 2 "${file}" | tail -n 1)
 	l2start=${line2:0:${LEXPECT}}
 	if [ "${EXPECT}" != "${l2start}" ]; then
 		echo "no copyright notice in: $file"
