43249f1d73c63dc3cfb52b3edd742bdb3c2dddb4
philipp <philipp@71d118b0-3217-0410-8c98-bf3badd46c86>
2012-05-12 21:18:30 +0000

    add utility methods for starting threads which should run forever and be restarted when they die

--- src/main/java/ch/bind/philib/lang/ThreadUtil.java
+++ src/main/java/ch/bind/philib/lang/ThreadUtil.java
@@ -21,33 +21,97 @@
  */
 package ch.bind.philib.lang;
 
+import java.util.concurrent.atomic.AtomicLong;
+
+import ch.bind.philib.validation.SimpleValidation;
+
 public final class ThreadUtil {
 
-    private static final PhiLog LOG = new PhiLog(ThreadUtil.class);
+	private static final PhiLog LOG = new PhiLog(ThreadUtil.class);
 
-    public static final long DEFAULT_WAIT_TIME_MS = 1000L;
+	public static final long DEFAULT_WAIT_TIME_MS = 1000L;
 
-    public static boolean interruptAndJoin(Thread t) {
-        return interruptAndJoin(t, DEFAULT_WAIT_TIME_MS);
-    }
+	private ThreadUtil() {
+	}
 
-    public static boolean interruptAndJoin(Thread t, long waitTime) {
-        if (t == null)
-            return true;
-        if (!t.isAlive())
-            return true;
+	public static boolean interruptAndJoin(Thread t) {
+		return interruptAndJoin(t, DEFAULT_WAIT_TIME_MS);
+	}
 
-        t.interrupt();
-        try {
-            t.join(waitTime);
-        } catch (InterruptedException e) {
-            LOG.warn("interrupted while waiting for a thread to finish: " + e.getMessage(), e);
-        }
-        if (t.isAlive()) {
-            LOG.warn("thread is still alive: " + t.getName());
-            return false;
-        } else {
-            return true;
-        }
-    }
+	public static boolean interruptAndJoin(Thread t, long waitTime) {
+		if (t == null)
+			return true;
+		if (!t.isAlive())
+			return true;
+
+		t.interrupt();
+		try {
+			t.join(waitTime);
+		} catch (InterruptedException e) {
+			LOG.warn("interrupted while waiting for a thread to finish: " + e.getMessage(), e);
+		}
+		if (t.isAlive()) {
+			LOG.warn("thread is still alive: " + t.getName());
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	private static final AtomicLong FOREVER_RUNNER_SEQ = new AtomicLong(1);
+
+	private static final String FOREVER_RUNNER_NAME_FMT = "%s-for-%s-%d";
+
+	public static Thread runForever(Runnable runnable) {
+		SimpleValidation.notNull(runnable);
+		String threadName = String.format(FOREVER_RUNNER_NAME_FMT, //
+				ForeverRunner.class.getSimpleName(), runnable.getClass().getSimpleName(), FOREVER_RUNNER_SEQ.getAndIncrement());
+		return runForever(runnable, threadName);
+	}
+
+	public static Thread runForever(Runnable runnable, String threadName) {
+		return runForever(null, runnable, threadName);
+	}
+
+	public static Thread runForever(ThreadGroup group, Runnable runnable, String threadName) {
+		// stackSize of 0 stands for: ignore this parameter
+		return runForever(null, runnable, threadName, 0);
+	}
+
+	public static Thread runForever(ThreadGroup group, Runnable runnable, String threadName, long stackSize) {
+		SimpleValidation.notNull(runnable);
+		SimpleValidation.notNull(threadName);
+		ForeverRunner runner = new ForeverRunner(threadName, runnable);
+		Thread t = new Thread(group, runner, threadName, stackSize);
+		t.start();
+		return t;
+	}
+
+	private static final class ForeverRunner implements Runnable {
+
+		private final String threadName;
+
+		private final Runnable runnable;
+
+		public ForeverRunner(String threadName, Runnable runnable) {
+			this.threadName = threadName;
+			this.runnable = runnable;
+		}
+
+		@Override
+		public void run() {
+			while (true) {
+				try {
+					runnable.run();
+				} catch (Exception e) {
+					if (e instanceof InterruptedException) {
+						// only shut down when we have a lawfull shutdown
+						return;
+					} else {
+						LOG.info("runnable crashed, restarting it. thread-name=" + threadName, e);
+					}
+				}
+			}
+		}
+	}
 }
