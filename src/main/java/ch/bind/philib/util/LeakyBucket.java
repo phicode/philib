89124cc55dbc4657a8d27acd351d834250f6af41
philipp <philipp@71d118b0-3217-0410-8c98-bf3badd46c86>
2012-06-07 15:53:49 +0000

    prevent divide by zero

--- src/main/java/ch/bind/philib/util/LeakyBucket.java
+++ src/main/java/ch/bind/philib/util/LeakyBucket.java
@@ -25,101 +25,104 @@ import ch.bind.philib.validation.SimpleValidation;
 
 public final class LeakyBucket {
 
-    private final long capacity;
+	private final long capacity;
 
-    private final long releaseIntervalNs;
+	private final long releaseIntervalNs;
 
-    private long lastReleaseNs;
+	private long lastReleaseNs;
 
-    private long numLeases;
+	private long numLeases;
 
-    private LeakyBucket(long releaseIntervalNs, long capacity) {
-        this.releaseIntervalNs = releaseIntervalNs;
-        this.capacity = capacity;
-    }
+	private LeakyBucket(long releaseIntervalNs, long capacity) {
+		this.releaseIntervalNs = releaseIntervalNs;
+		this.capacity = capacity;
+	}
 
-    /**
-     * @param releasePerSecond
-     * @param capacity
-     * @return
-     */
-    public static LeakyBucket withReleasePerSecond(double releasePerSecond, long capacity) {
-        SimpleValidation.isTrue(releasePerSecond >= 0.000001, "releasePerSecond must be >= 0.000001");
-        SimpleValidation.isTrue(capacity >= 1, "capacity must be >= 1");
-        long releaseIntervalNano = (long) Math.ceil(1000000000f / releasePerSecond);
-        return new LeakyBucket(releaseIntervalNano, capacity);
-    }
+	/**
+	 * @param releasePerSecond
+	 * @param capacity
+	 * @return
+	 */
+	public static LeakyBucket withReleasePerSecond(double releasePerSecond, long capacity) {
+		SimpleValidation.isTrue(releasePerSecond >= 0.000001, "releasePerSecond must be >= 0.000001");
+		SimpleValidation.isTrue(capacity >= 1, "capacity must be >= 1");
+		long releaseIntervalNano = (long) Math.ceil(1000000000f / releasePerSecond);
+		return new LeakyBucket(releaseIntervalNano, capacity);
+	}
 
-    public static LeakyBucket withReleaseIntervalNano(long releaseIntervalNano, long capacity) {
-        SimpleValidation.isTrue(releaseIntervalNano >= 0, "releaseIntervalNano must be > 0");
-        SimpleValidation.isTrue(capacity >= 1, "capacity must be >= 1");
-        return new LeakyBucket(releaseIntervalNano, capacity);
-    }
+	public static LeakyBucket withReleaseIntervalNano(long releaseIntervalNano, long capacity) {
+		SimpleValidation.isTrue(releaseIntervalNano > 0, "releaseIntervalNano must be > 0");
+		SimpleValidation.isTrue(capacity >= 1, "capacity must be >= 1");
+		return new LeakyBucket(releaseIntervalNano, capacity);
+	}
 
-    public long getCapacity() {
-        return capacity;
-    }
+	public long getCapacity() {
+		return capacity;
+	}
 
-    public void acquire(long amount) {
-        acquire(amount, System.nanoTime());
-    }
+	public void acquire(long amount) {
+		acquire(amount, System.nanoTime());
+	}
 
-    public void acquire(long amount, long timeNs) {
-        recalc(timeNs);
-        numLeases -= amount;
-    }
+	public void acquire(long amount, long timeNs) {
+		recalc(timeNs);
+		numLeases -= amount;
+	}
 
-    public long available() {
-        return available(System.nanoTime());
-    }
+	public long available() {
+		return available(System.nanoTime());
+	}
 
-    public long available(long timeNs) {
-        recalc(timeNs);
-        return numLeases;
-    }
+	public long available(long timeNs) {
+		recalc(timeNs);
+		return numLeases;
+	}
 
-    public long nextAvailableNano() {
-        return nextAvailableNano(System.nanoTime());
-    }
+	public long nextAvailableNano() {
+		return nextAvailableNano(System.nanoTime());
+	}
 
-    public long nextAvailableNano(long timeNs) {
-        recalc(timeNs);
-        if (numLeases > 0) {
-            // available immediately
-            return 0;
-        } else {
-            long nextAvailNano = lastReleaseNs + releaseIntervalNs;
-            return nextAvailNano - timeNs;
-        }
-    }
+	public long nextAvailableNano(long timeNs) {
+		recalc(timeNs);
+		if (numLeases > 0) {
+			// available immediately
+			return 0;
+		}
+		else {
+			long nextAvailNano = lastReleaseNs + releaseIntervalNs;
+			return nextAvailNano - timeNs;
+		}
+	}
 
-    public void sleepWhileNoneAvailable() throws InterruptedException {
-        long nextAvailNano = nextAvailableNano();
-        while (nextAvailNano > 0) {
-            long sleepMs = nextAvailNano / 1000000L;
-            int sleepNano = (int) (nextAvailNano % 1000000L);
-            Thread.sleep(sleepMs, sleepNano);
-            nextAvailNano = nextAvailableNano();
-        }
-    }
+	public void sleepWhileNoneAvailable() throws InterruptedException {
+		long nextAvailNano = nextAvailableNano();
+		while (nextAvailNano > 0) {
+			long sleepMs = nextAvailNano / 1000000L;
+			int sleepNano = (int) (nextAvailNano % 1000000L);
+			Thread.sleep(sleepMs, sleepNano);
+			nextAvailNano = nextAvailableNano();
+		}
+	}
 
-    private void recalc(final long timeNs) {
-        if (timeNs < lastReleaseNs) {
-            // it seems that someone adjusted his clock backwards
-            lastReleaseNs = timeNs;
-        } else {
-            long elapsedNs = timeNs - lastReleaseNs;
-            long numRelease = elapsedNs / releaseIntervalNs;
-            long newLeases = numLeases + numRelease;
+	private void recalc(final long timeNs) {
+		if (timeNs < lastReleaseNs) {
+			// it seems that someone adjusted his clock backwards
+			lastReleaseNs = timeNs;
+		}
+		else {
+			long elapsedNs = timeNs - lastReleaseNs;
+			long numRelease = elapsedNs / releaseIntervalNs;
+			long newLeases = numLeases + numRelease;
 
-            // dont go over the limit
-            if (newLeases > capacity) {
-                numLeases = capacity;
-                lastReleaseNs = timeNs;
-            } else {
-                numLeases = newLeases;
-                lastReleaseNs += (numRelease * releaseIntervalNs);
-            }
-        }
-    }
+			// dont go over the limit
+			if (newLeases > capacity) {
+				numLeases = capacity;
+				lastReleaseNs = timeNs;
+			}
+			else {
+				numLeases = newLeases;
+				lastReleaseNs += (numRelease * releaseIntervalNs);
+			}
+		}
+	}
 }
