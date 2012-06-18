68c54828139db7635684d1d09116286292e147da
philipp <philipp@71d118b0-3217-0410-8c98-bf3badd46c86>
2012-06-18 23:32:16 +0000

    copyright header

--- src/main/java/ch/bind/philib/net/tcp/TcpServerEventHandler.java
+++ src/main/java/ch/bind/philib/net/tcp/TcpServerEventHandler.java
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2012 Philipp Meinen <philipp@bind.ch>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software
+ * is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+ * THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 package ch.bind.philib.net.tcp;
 
 import java.io.IOException;
@@ -10,38 +31,38 @@ import ch.bind.philib.validation.Validation;
 
 class TcpServerEventHandler extends EventHandlerBase {
 
-	private final ServerSocketChannel channel;
+    private final ServerSocketChannel channel;
 
-	private final TcpServer server;
+    private final TcpServer server;
 
-	TcpServerEventHandler(ServerSocketChannel channel, TcpServer server) {
-		super();
-		Validation.notNull(channel);
-		Validation.notNull(server);
-		this.channel = channel;
-		this.server = server;
-	}
+    TcpServerEventHandler(ServerSocketChannel channel, TcpServer server) {
+        super();
+        Validation.notNull(channel);
+        Validation.notNull(server);
+        this.channel = channel;
+        this.server = server;
+    }
 
-	@Override
-	public SelectableChannel getChannel() {
-		return channel;
-	}
+    @Override
+    public SelectableChannel getChannel() {
+        return channel;
+    }
 
-	@Override
-	public void close() throws IOException {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("TODO");
-	}
+    @Override
+    public void close() throws IOException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("TODO");
+    }
 
-	@Override
-	public void handleAccept() throws IOException {
-		while (true) {
-			SocketChannel clientChannel = channel.accept();
-			if (clientChannel == null) {
-				// no more connections to accept
-				return;
-			}
-			server.createSession(clientChannel);
-		}
-	}
+    @Override
+    public void handleAccept() throws IOException {
+        while (true) {
+            SocketChannel clientChannel = channel.accept();
+            if (clientChannel == null) {
+                // no more connections to accept
+                return;
+            }
+            server.createSession(clientChannel);
+        }
+    }
 }
