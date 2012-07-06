097e7efdcab9014e0ef74ff3e4c9c8783e0eec9a
philipp <philipp@71d118b0-3217-0410-8c98-bf3badd46c86>
2012-07-06 23:04:25 +0000

    bourne shell compatible

--- checkHeaders.sh
+++ checkHeaders.sh
@@ -1,14 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
 EXPECT=" * Copyright (c) "
 LEXPECT=${#EXPECT}
 
 for file in $(find . -type f -name "*.java"); do
-
 	line2=$(head -n 2 "${file}" | tail -n 1)
-	l2start=${line2:0:${LEXPECT}}
+	l2start=$(expr substr "$line2" 1 ${LEXPECT})
 	if [ "${EXPECT}" != "${l2start}" ]; then
 		echo "no copyright notice in: $file"
 	fi
-
 done
