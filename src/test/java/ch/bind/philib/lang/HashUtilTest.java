f76ca022c091e929e48893492174efc1e8fa405f
philipp <philipp@71d118b0-3217-0410-8c98-bf3badd46c86>
2012-10-13 22:08:48 +0000

    collision test

--- src/test/java/ch/bind/philib/lang/HashUtilTest.java
+++ src/test/java/ch/bind/philib/lang/HashUtilTest.java
@@ -22,6 +22,9 @@
 package ch.bind.philib.lang;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import java.util.Arrays;
 
 import org.testng.annotations.Test;
 
@@ -261,4 +264,56 @@ public class HashUtilTest {
 		assertEquals(expected, hash);
 	}
 
+	@Test
+	public void differentHashes() {
+		// all permutations of a:64b, b:32b, c:16b, d:8b
+		// where a := [ 3 .. 300 @ step 3 ]
+		// where b := [ 8 .. 800 @ step 8 ]
+		// where c := [ 9 .. 900 @ step 9 ]
+		// where d := [ 1 .. 255 @ step 2]
+		// space: 100 * 100 * 100 * 128 = 128'000'000 * 4B = 512MiB
+		final int n = 100 * 100 * 100 * 128;
+		final int[] results = new int[n];
+		int idx = 0;
+		final long tStart = System.currentTimeMillis();
+		for (long a = 3; a <= 300; a += 3) {
+			for (int b = 8; b <= 800; b += 8) {
+				for (short c = 9; c <= 900; c += 9) {
+					for (int id = 1; id < 256; id += 2) {
+						byte d = (byte) (id & 0xFF);
+						int hc = hash(a, b, c, d);
+						results[idx++] = hc;
+					}
+				}
+			}
+		}
+		assertEquals(idx, n);
+		final long t0 = System.currentTimeMillis() - tStart;
+		Arrays.sort(results);
+		final long t1 = System.currentTimeMillis() - tStart - t0;
+		int prev = results[0];
+
+		int colls = 0;
+		for (int i = 1; i < n; i++) {
+			int cur = results[i];
+			if (cur == prev) {
+				colls++;
+			}
+			prev = cur;
+			cur++;
+		}
+		final long t2 = System.currentTimeMillis() - tStart - t0 - t1;
+		double collFactor = ((double) colls) / ((double) n);
+		// less then 1.5%
+		assertTrue(collFactor < 0.015);
+		System.out.printf("t0=%d, t1=%d, t2=%d, collisions=%d/%d\n", t0, t1, t2, colls, n);
+	}
+
+	private static final int hash(long a, int b, short c, byte d) {
+		int h = HashUtil.startHash(a);
+		h = HashUtil.nextHash(h, b);
+		h = HashUtil.nextHash(h, c);
+		h = HashUtil.nextHash(h, d);
+		return h;
+	}
 }
