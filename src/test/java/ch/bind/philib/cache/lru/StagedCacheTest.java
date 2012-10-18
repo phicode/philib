88f82878420aa5f90bfca6102a1c95423e8a8133
philipp <philipp@71d118b0-3217-0410-8c98-bf3badd46c86>
2012-10-18 20:59:15 +0000

    not yet successful test

--- src/test/java/ch/bind/philib/cache/lru/StagedCacheTest.java
+++ src/test/java/ch/bind/philib/cache/lru/StagedCacheTest.java
@@ -1,5 +1,9 @@
 package ch.bind.philib.cache.lru;
 
+import static org.testng.Assert.*;
+
+import org.testng.annotations.Test;
+
 public class StagedCacheTest extends CacheTestBase {
 
 	@Override
@@ -20,5 +24,37 @@ public class StagedCacheTest extends CacheTestBase {
 	@Override
 	int getDefaultCapacity() {
 		return StagedCache.DEFAULT_CACHE_CAPACITY;
+	}
+
+	@Test
+	public void stages() {
+		final int cap = 100000;
+		Cache<Integer, Integer> cache = new StagedCache<Integer, Integer>(cap, 0.5, 2);
+		for (int i = 0; i < cap; i++) {
+			cache.add(i, i * i);
+		}
+
+		// 1 hit each
+		for (int i = 0; i < cap; i++) {
+			assertEquals(cache.get(i), Integer.valueOf(i * i));
+		}
+
+		// 2 hits for 0-49999
+		for (int i = 0; i < 50000; i++) {
+			assertEquals(cache.get(i), Integer.valueOf(i * i));
+		}
+
+		// add 100000-149999
+		for (int i = 100000; i < 150000; i++) {
+			cache.add(i, i * i);
+		}
+
+		for (int i = -10000; i < 200000; i++) {
+			if ((i >= 0 && i < 50000) || (i >= 100000 && i < 150000)) {
+				assertEquals(cache.get(i), Integer.valueOf(i * i));
+			} else {
+				assertNull(cache.get(i));
+			}
+		}
 	}
 }
