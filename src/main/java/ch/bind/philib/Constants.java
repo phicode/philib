089dc7a947ad77f79d202b02ee7b85b28257e6c2
philipp <philipp@71d118b0-3217-0410-8c98-bf3badd46c86>
2012-10-20 11:09:50 +0000

    increment project minor version

--- src/main/java/ch/bind/philib/Constants.java
+++ src/main/java/ch/bind/philib/Constants.java
@@ -33,7 +33,7 @@ public final class Constants {
 
 	public static final int PHILIB_VERSION_MAJOR = 0;
 
-	public static final int PHILIB_VERSION_MINOR = 3;
+	public static final int PHILIB_VERSION_MINOR = 4;
 
 	public static final int PHILIB_VERSION_PATCH = 0;
 
