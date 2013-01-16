43da0318390866c5d150413a27249a9a5a72062e
philipp <philipp@71d118b0-3217-0410-8c98-bf3badd46c86>
2013-01-16 22:20:11 +0000

    rename the constants class since it only hold the philib constants

--- src/main/java/ch/bind/philib/Philib.java
+++ src/main/java/ch/bind/philib/Philib.java
@@ -23,19 +23,19 @@
 package ch.bind.philib;
 
 /**
- * Public constants like the library version.
+ * Philib constants like the library version.
  * 
  * @author Philipp Meinen
  */
-public final class Constants {
+public final class Philib {
 
-	private Constants() {}
+	private Philib() {}
 
 	public static final int PHILIB_VERSION_MAJOR = 0;
 
-	public static final int PHILIB_VERSION_MINOR = 6;
+	public static final int PHILIB_VERSION_MINOR = 5;
 
-	public static final int PHILIB_VERSION_PATCH = 0;
+	public static final int PHILIB_VERSION_PATCH = 1;
 
 	public static final String PHILIB_VERSION = PHILIB_VERSION_MAJOR + "." + PHILIB_VERSION_MINOR + "." + PHILIB_VERSION_PATCH;
 
