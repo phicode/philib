7b7f362acbb455d6453fa2b2c7d8c01686cd1609
philipp <philipp@71d118b0-3217-0410-8c98-bf3badd46c86>
2013-02-19 11:58:44 +0000

    check for null names

--- src/main/java/ch/bind/philib/lang/NamedSeqThreadFactory.java
+++ src/main/java/ch/bind/philib/lang/NamedSeqThreadFactory.java
@@ -1,7 +1,31 @@
+/*
+ * Copyright (c) 2013 Philipp Meinen <philipp@bind.ch>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software
+ * is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+ * THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package ch.bind.philib.lang;
 
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.atomic.AtomicLong;
+
+import ch.bind.philib.validation.Validation;
 
 /**
  * A thread factory which generates thread names of the form
@@ -20,6 +44,7 @@ public final class NamedSeqThreadFactory implements ThreadFactory {
 	 * @param name The name which must be used for newly created threads.
 	 */
 	public NamedSeqThreadFactory(String name) {
+		Validation.notNull(name);
 		this.name = name;
 	}
 
