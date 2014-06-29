556e528a418b6ad9a4d0425a1176695d71f4137e
Philipp Meinen <philipp@bind.ch>
2014-06-29 20:15:25 +0200

    cache: fix unit test

--- src/test/java/ch/bind/philib/cache/CacheTestBase.java
+++ src/test/java/ch/bind/philib/cache/CacheTestBase.java
@@ -33,6 +33,7 @@ import java.security.SecureRandom;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertNotSame;
 import static org.testng.Assert.assertNull;
 import static org.testng.Assert.assertTrue;
 
