87dfa2cadd01e82bee438f3049fdabe61c246e3d
Philipp Meinen <philipp@bind.ch>
2022-06-09 10:55:07 +0200

    update dependencies

--- src/test/java/ch/bind/philib/io/SafeCloseUtilTest.java
+++ src/test/java/ch/bind/philib/io/SafeCloseUtilTest.java
@@ -1,16 +1,16 @@
 /*
  * Copyright (c) 2013 Philipp Meinen <philipp@bind.ch>
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"),
  * to deal in the Software without restriction, including without limitation
  * the rights to use, copy, modify, merge, publish, distribute, sublicense,
  * and/or sell copies of the Software, and to permit persons to whom the Software
  * is furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included
  * in all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
  * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
@@ -22,20 +22,20 @@
 
 package ch.bind.philib.io;
 
-import org.slf4j.Logger;
-import org.slf4j.helpers.NOPLogger;
-import org.testng.annotations.Test;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.testng.Assert.assertEquals;
 
 import java.io.Closeable;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
-import static org.testng.Assert.assertEquals;
+import org.slf4j.Logger;
+import org.slf4j.helpers.NOPLogger;
+import org.testng.annotations.Test;
 
 public class SafeCloseUtilTest {
 
